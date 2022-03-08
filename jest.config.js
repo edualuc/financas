f49bacdf3d7c9394f9615f5d70e0f79ebe130408
@edualuc Eduardo Lucas <edualuc@gmail.com>
2022-03-08 01:34:25 -0300

    chore: add ts-jest

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,32 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
+  // Automatically clear mock calls, instances and results before every test
+  clearMocks: true,
+
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: true,
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: "coverage",
+
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: "babel",
+
+  coverageThreshold: {
+    global: {
+      branches: 0,
+      functions: 0,
+      lines: 0,
+      statements: 0,
+    },
+    "./**/*.ts": {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  },
+
   roots: ["<rootDir>/src"],
   testEnvironment: "node",
   transform: {
